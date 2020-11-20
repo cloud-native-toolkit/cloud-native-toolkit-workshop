7b2609821f5df76c06926942d2590f85321fbb04
Carlos Santana <csantana23@gmail.com>
2020-11-19 22:52:35 -0500

    clean user creation script

--- scripts/03-ocp-users.sh
+++ scripts/03-ocp-users.sh
@@ -11,12 +11,9 @@ HTPASSWD_FILE="${SRC_DIR}/../local/${HTPASSWD_FILENAME}"
 COUNT_USERS=${COUNT_USERS:-15}
 
 rm "${HTPASSWD_FILE}" || true
+touch "${HTPASSWD_FILE}"
 for (( c=1; c<=COUNT_USERS; c++ )); do
-  FLAGS="-B -b"
-  if [[ "${c}" -eq "1" ]]; then
-    FLAGS="${FLAGS} -c"
-  fi
-  htpasswd ${FLAGS} "${HTPASSWD_FILE}" user${c} user${c}
+  htpasswd -B -b "${HTPASSWD_FILE}" user${c} user${c}
 done
 
 
