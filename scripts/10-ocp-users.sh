4a2e7484c57e1ce80c0d0c8f86597b0dc296f0aa
Carlos Santana <csantana23@gmail.com>
2020-12-17 14:10:37 -0500

    fix use case when oauth is not set

--- scripts/10-ocp-users.sh
+++ scripts/10-ocp-users.sh
@@ -7,6 +7,7 @@ SRC_DIR="$(cd "${SCRIPT_DIR}"; pwd -P)"
 
 HTPASSWD_FILENAME=${HTPASSWD_FILENAME:-htpasswd}
 TOOLKIT_SECRET=${TOOLKIT_SECRET:-ibm-toolkit-htpasswd}
+TOOLKIT_PROVIDER_NAME=${TOOLKIT_PROVIDER_NAME:ibm-toolkit}
 # password is the password :-)
 HTPASSWD_HASH='$2y$05$.juxXTUzqc5wzUlCcZl1puKGI9YDOLhFV7.HFtYi1GdD.DKG32D0.'
 COUNT_USERS=${COUNT_USERS:-15}
@@ -26,10 +27,29 @@ done
 
 oc delete secret ${TOOLKIT_SECRET} -n openshift-config 2>/dev/null || true
 oc create secret generic ${TOOLKIT_SECRET} -n openshift-config --from-file=htpasswd=${HTPASSWD_FILENAME}
-oauth=$(oc get OAuth cluster -o yaml)
-if [[ ! "${oauth}" =~ "${TOOLKIT_SECRET}" ]]; then
+OAUTH_SPEC=$(oc get OAuth cluster -o json | jq .spec)
+
+if [[ "${OAUTH_SPEC}" == '{}' ]]; then
+  echo "No spec configure for OAuth"
+oc replace -f - <<EOF
+apiVersion: config.openshift.io/v1
+kind: OAuth
+metadata:
+  annotations:
+    release.openshift.io/create-only: "true"
+  name: cluster
+spec:
+  identityProviders:
+  - name: ${TOOLKIT_PROVIDER_NAME}
+    mappingMethod: claim
+    type: HTPasswd
+    htpasswd:
+      fileData:
+        name: ${TOOLKIT_SECRET}
+EOF
+elif [[ ! "${oauth}" =~ "${TOOLKIT_SECRET}" ]]; then
   echo "updating OAuth with toolkit users"
-  oc patch OAuth cluster --type json -p "[{\"op\":\"add\",\"path\":\"/spec/identityProviders/-\",\"value\":{\"htpasswd\":{\"fileData\":{\"name\":\"${TOOLKIT_SECRET}\"}},\"mappingMethod\":\"claim\",\"name\":\"ibm-toolkit\",\"type\":\"HTPasswd\"} }]"
+  oc patch OAuth cluster --type json -p "[{\"op\":\"add\",\"path\":\"/spec/identityProviders/-\",\"value\":{\"htpasswd\":{\"fileData\":{\"name\":\"${TOOLKIT_SECRET}\"}},\"mappingMethod\":\"claim\",\"name\":\"${TOOLKIT_PROVIDER_NAME}\",\"type\":\"HTPasswd\"} }]"
 fi
 
 popd
