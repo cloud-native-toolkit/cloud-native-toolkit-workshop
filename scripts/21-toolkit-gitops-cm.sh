dac888081aa2c0d4e2194193caa4798aac9b2d73
Carlos Santana <csantana23@gmail.com>
2021-01-12 00:25:45 -0500

    master branch for gitops

--- scripts/21-toolkit-gitops-cm.sh
+++ scripts/21-toolkit-gitops-cm.sh
@@ -7,7 +7,7 @@ SRC_DIR="$(cd "${SCRIPT_DIR}"; pwd -P)"
 
 TOOLKIT_NAMESPACE=${TOOLKIT_NAMESPACE:-tools}
 TOOLKIT_GITOPS_HOST=$(oc get route -n ${TOOLKIT_NAMESPACE} gogs --template='{{.spec.host}}')
-TOOLKIT_GITOPS_BRANCH=${TOOLKIT_GITOPS_BRANCH:-qa}
+TOOLKIT_GITOPS_BRANCH=${TOOLKIT_GITOPS_BRANCH:-master}
 TOOLKIT_GITOPS_ORG=${TOOLKIT_GITOPS_ORG:-toolkit}
 TOOLKIT_GITOPS_REPO=${TOOLKIT_GITOPS_REPO:-gitops}
 TOOLKIT_GITOPS_PROTOCOL=${TOOLKIT_GITOPS_PROTOCOL:-http}
@@ -15,7 +15,7 @@ TOOLKIT_GITOPS_PROTOCOL=${TOOLKIT_GITOPS_PROTOCOL:-http}
 oc delete cm gitops-repo -n ${TOOLKIT_NAMESPACE} 2>/dev/null || true
 
 oc create cm gitops-repo \
---from-literal parentdir="bash -c 'basename \${NAMESPACE} -dev'" \
+--from-literal parentdir="bash -c 'echo qa/\$(basename \${NAMESPACE} -dev)'" \
 --from-literal host=${TOOLKIT_GITOPS_HOST} \
 --from-literal branch=${TOOLKIT_GITOPS_BRANCH} \
 --from-literal org=${TOOLKIT_GITOPS_ORG} \
