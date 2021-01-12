d1670dca025317415a21141c5d7a7d9e139cbd5a
Carlos Santana <csantana@us.ibm.com>
2021-01-12 18:24:14 -0500

    add git ops console link (#13)

--- scripts/24-toolkit-console-git.sh
+++ scripts/24-toolkit-console-git.sh
@@ -2,13 +2,16 @@
 
 set -euo pipefail
 
-SCRIPT_DIR=$(dirname "$0")
-SRC_DIR="$(cd "${SCRIPT_DIR}"; pwd -P)"
-
 TOOLKIT_NAMESPACE=${TOOLKIT_NAMESPACE:-tools}
 GIT_PROTOCOL="https"
 GIT_HOST=$(oc get route -n ${TOOLKIT_NAMESPACE} gogs --template='{{.spec.host}}')
+GIT_ORG=${GIT_ORG:-toolkit}
+GIT_REPO=${GIT_REPO:-gitops}
 GIT_URL="${GIT_PROTOCOL}://${GIT_HOST}"
-GIT_NAME="Git"
+GIT_URL_OPS="${GIT_PROTOCOL}://${GIT_HOST}/${GIT_ORG}/${GIT_REPO}"
+GIT_NAME="Git Dev"
+GIT_NAME_OPS="Git Ops"
 
 oc patch consolelink toolkit-sourcecontrol --type merge -p "{\"spec\": {\"href\": \"${GIT_URL}\", \"text\": \"${GIT_NAME}\"}}"
+oc delete consolelink ${GIT_NAME_OPS} || true
+oc get consolelink toolkit-sourcecontrol -o yaml | sed 's/name: toolkit-sourcecontrol/name: toolkit-gitops/' | sed 's/text: ${GIT_NAME}/text: ${GIT_NAME_OPS}/' | oc apply -f -
