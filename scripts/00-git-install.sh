fdd8bdbecca3aaa9d3cbfffcd3d67fbe5ab5b892
Larry Steck <lsteck@us.ibm.com>
2021-10-28 15:43:42 -0500

    install gittea operator working
    Signed-off-by: Larry Steck <lsteck@us.ibm.com>

--- scripts/00-git-install.sh
+++ scripts/00-git-install.sh
@@ -3,31 +3,79 @@
 set -euo pipefail
 
 TOOLKIT_NAMESPACE=${TOOLKIT_NAMESPACE:-tools}
-GOGS_YAML=${GOGS_YAML:-https://raw.githubusercontent.com/csantanapr/gogs/workshop/gogs-template.yaml}
-OCP_DEFAULT_APP_DOMAIN=$(oc get ingresses.config.openshift.io cluster -o template={{.spec.domain}})
-GIT_PROTOCOL=${GIT_PROTOCOL:-http}
-GIT_HOST=gogs-${TOOLKIT_NAMESPACE}.${OCP_DEFAULT_APP_DOMAIN}
-GIT_URL="${GIT_PROTOCOL}://${GIT_HOST}"
 GIT_CRED_USERNAME=${GIT_CRED_USERNAME:-toolkit}
 GIT_CRED_PASSWORD=${GIT_CRED_PASSWORD:-toolkit}
-GOGS_POST_FORM="user_name=${GIT_CRED_USERNAME}&password=${GIT_CRED_PASSWORD}&retype=${GIT_CRED_PASSWORD}&email=toolkit%40cloudnativetoolkit.dev"
 
-status=$(oc get pods -n ${TOOLKIT_NAMESPACE} -l app=gogs)
-if [[ "$status" =~ "Running" ]]; then
-  echo "gogs git server already installed"
-  exit 0
+OPERATOR_NAME="gitea-operator"
+OPERATOR_NAMESPACE="openshift-operators"
+DEPLOYMENT="${OPERATOR_NAME}-controller-manager"
+INSTANCE_NAME="gitea-tools"
+
+if [[ $(oc get subscription ${OPERATOR_NAME} -n ${OPERATOR_NAMESPACE} 2> /dev/null) ]]; then
+  echo "Gitea operator already installed"
+else
+  echo "Install gitea operator"
+  helm template ${OPERATOR_NAME} gitea-operator --repo "https://lsteck.github.io/toolkit-charts" | kubectl apply --validate=false -f -
+
+  # Wait for Deployment
+  count=0
+  until kubectl get deployment "${DEPLOYMENT}" -n "${OPERATOR_NAMESPACE}" 1> /dev/null 2> /dev/null ;
+  do
+    if [[ ${count} -eq 24 ]]; then
+      echo "Timed out waiting for deployment/${DEPLOYMENT} in ${OPERATOR_NAMESPACE} to start"
+      kubectl get deployment "${DEPLOYMENT}" -n "${OPERATOR_NAMESPACE}" 
+      exit 1
+    else
+      count=$((count + 1))
+    fi
+
+    echo "Waiting for deployment/${DEPLOYMENT} in ${OPERATOR_NAMESPACE} to start"
+    sleep 10
+  done
+
+  if kubectl get deployment "${DEPLOYMENT}" -n "${OPERATOR_NAMESPACE}" 1> /dev/null 2> /dev/null; then
+    kubectl rollout status deployment "${DEPLOYMENT}" -n "${OPERATOR_NAMESPACE}"
+  fi
+
+  # Wait for Pods
+  count=0
+  while kubectl get pods -o jsonpath='{range .items[*]}{.status.phase}{"\n"}{end}' -n "${OPERATOR_NAMESPACE}" | grep -q Pending; do
+    if [[ ${count} -eq 24 ]]; then
+      echo "Timed out waiting for pods in ${OPERATOR_NAMESPACE} to start"
+      kubectl get pods -n "${OPERATOR_NAMESPACE}"
+      exit 1
+    else
+      count=$((count + 1))
+    fi
+
+    echo "Waiting for all pods in ${NAMESPACE} to start"
+    sleep 10
+  done
 fi
 
-oc new-app \
--n ${TOOLKIT_NAMESPACE} \
--f ${GOGS_YAML} \
---param=HOSTNAME=${GIT_HOST}
+
+status=$(oc get pods -n ${TOOLKIT_NAMESPACE} -l app=gitea-tools 2> /dev/null)
+if [[ "$status" =~ "Running" ]]; then
+  echo "Gitea server already installed"
+else
+  echo "Install Gitea server"
+
+fi
 
 
-oc rollout status dc/gogs -n ${TOOLKIT_NAMESPACE}
+echo "Checking for toolkit admin account"
+# Create toolkit admin user if needed.
+#GIT_CRED_USERNAME
+admin_user=$(oc get secret gitea-tools-access -n tools -o go-template --template="{{.data.username|base64decode}}")
+echo ${admin_user}
+echo ${GIT_CRED_USERNAME}
+if [${GIT_CRED_USERNAME} == ${admin_user}]; then
+  echo "toolkit admin account exists"
+else
+  echo "Creating toolkit admin account"
+fi
 
-echo "creating admin user ${GIT_CRED_USERNAME}"
-curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "${GOGS_POST_FORM}" "${GIT_URL}/user/sign_up"
 
-echo "creating access token toolkit for user ${GIT_CRED_USERNAME}"
-curl -X POST -H "Content-Type: application/json" -d "{ \"name\": \"toolkit\" }" "${GIT_PROTOCOL}://${GIT_CRED_USERNAME}:${GIT_CRED_PASSWORD}@${GIT_HOST}/api/v1/users/${GIT_CRED_USERNAME}/tokens"
+
+
+
